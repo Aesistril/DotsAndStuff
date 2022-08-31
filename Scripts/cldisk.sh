6d3d0ff24fefbfa65147374be865917d9d296620
Aesistril <yigit@yigitayaz.xyz>
2022-08-31 21:58:35 +0300

    Added bottles temp directory

--- Scripts/cldisk.sh
+++ Scripts/cldisk.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 du -sh /home/*/.cache/
 du -sh /var/cache/pacman/pkg/
+du -sh /home/*/.local/share/bottles/temp/
 
 read -p "Do you want to delete these files (y/n)?" choice
 case "$choice" in 
-  y|Y ) sudo rm /var/cache/pacman/pkg/*;sudo rm -rf /home/*/.cache/*;;
+  y|Y ) sudo rm /var/cache/pacman/pkg/*;sudo rm -rf /home/*/.cache/*;rm -rf /home/*/.local/share/bottles/temp/*;;
   n|N ) echo "exiting";;
   * ) echo "invalid input";;
 esac
